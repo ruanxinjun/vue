5d706cf7beb500dfe2436f1f74aa61197e32a1ab
ruanjob <83932134@qq.com>
2017-11-18 22:29:21 +0800

    1

---
+++ shopping/admin/index.js
@@ -0,0 +1,13 @@
+import './admin.css';
+import './admin.less';
+
+document.getElementById("box").innerHTML=`
+
+	你妹的
+
+
+`
+
+var sum = (a,b)=>a+b;
+
+alert(sum(1025,2));
---
+++ shopping/consumer/index.js
@@ -0,0 +1,1 @@
+document.getElementById("box").innerHTML=" consumer page!!!"
